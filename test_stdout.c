2a21cc2ad4ae41a9cfb8452e93c6bad4344a8e5b
KenthJohan <kenth.johan.work@gmail.com>
2018-01-10 15:26:13 +0000

    test

--- test_stdout.c
+++ test_stdout.c
@@ -26,10 +26,13 @@
 #include <wiringPi.h>
 
 
+
+
+
 int I2C_Device;
 int SPI_Device;
 struct Lepton_Pixel_Grayscale16 Pixmap [Lepton3_Width * Lepton3_Height];
-size_t Segment_Per_Frame = 0;
+_Atomic size_t Safe_Counter = 0;
 
 
 void Lepton_App_Enable_Vsync (int Device, int Micro_Sleep, size_t Trial_Count)
@@ -42,6 +45,7 @@ void Lepton_App_Enable_Vsync (int Device, int Micro_Sleep, size_t Trial_Count)
    {
       Lepton_I2C_Execute 
       (
+         Lepton_Debug_None,
          Device, 
          Lepton_I2C_Command_GPIO_Mode_Set, 
          (void *) &(Mode),
@@ -62,46 +66,50 @@ void Interrupt_Handle ()
    switch (Result)
    {
       case Lepton_Stream_SPI_Error:
+      //printf ("Lepton_Stream_SPI_Error\n");break;
       case Lepton_Stream_Shifting:
+      //printf ("Lepton_Stream_Shifting\n");break;
       case Lepton_Stream_Invalid_Row:
+      //printf ("Lepton_Stream_Invalid_Row\n");break;
       case Lepton_Stream_Discard:
+      //printf ("Lepton_Stream_Discard\n");break;
       case Lepton_Stream_Mismatch:
+      //printf ("Lepton_Stream_Mismatch\n");break;
       case Lepton_Stream_Invalid_Segment:
+      //printf ("Lepton_Stream_Invalid_Segment\n");break;
       break;
    };
    
-   Segment_Per_Frame ++;
-   
    if (Result == 4)
    {
-      //Should be 4 segments but it is 12 due to US Export restriction.
-      Segment_Per_Frame = 0;
+      Safe_Counter = 0;
       
       int R = write (STDOUT_FILENO, Pixmap, sizeof (Pixmap));
       assert (R == sizeof (Pixmap));
    }
-   
-   //Check if the camera goes haywire.
-   //There should only be 4 segment (*3 if US Export restriction) per frame in Lepton 3.
-   if (Segment_Per_Frame > 300)
-   {
-      Segment_Per_Frame = 0;
-      Lepton_I2C_Write_Command (I2C_Device, Lepton_I2C_Command_Reboot);
-      sleep (3);
-      Lepton_App_Enable_Vsync (I2C_Device, 10, 10);
-   }
-   
-   
 }
 
 
 int main (int argc, char * argv [])
 { 
-   I2C_Device = Lepton_I2C_Open ("/dev/i2c-1");
+   I2C_Device = Lepton_I2C_Open (Lepton_Debug_None, "/dev/i2c-1");
+   //SPI_Device = Lepton_SPI_Open (Lepton_Debug0, "/dev/spidev0.0");
    SPI_Device = Lepton_SPI_Open ("/dev/spidev0.0");
-   int Status = Lepton_I2C_Status (I2C_Device);
-   Lepton_Strings_Base_printf (be16toh (Status), 10, 2, "Status: %10s\n");
    
+   {
+      int Status = Lepton_I2C_Status (Lepton_Debug_None, I2C_Device);
+      Lepton_Strings_Base_printf (stderr, be16toh (Status), 10, 2, "Status: %10s\n");
+   }
+   
+   Lepton_I2C_Write_Command (Lepton_Debug_None, I2C_Device, Lepton_I2C_Command_Reboot);
+   sleep (3);
+   Lepton_App_Enable_Vsync (I2C_Device, 10, 10);
+   
+   {
+      int Status = Lepton_I2C_Status (Lepton_Debug_None, I2C_Device);
+      Lepton_Strings_Base_printf (stderr, be16toh (Status), 10, 2, "Status: %10s\n");
+   }
+      
    {
       wiringPiSetup ();
       piHiPri (99);
@@ -112,8 +120,17 @@ int main (int argc, char * argv [])
    
    while (1)
    {
-      //printf ("S/P: %d\n", Segment_Per_Frame);
       sleep (1);
+      //Check if the camera goes haywire.
+      Safe_Counter ++;
+      if (Safe_Counter > 3)
+      {
+         Safe_Counter = 0;
+         fprintf (stderr, "\nCamera has gone haywire! Rebooting now to fix the problem.");
+         Lepton_I2C_Write_Command (Lepton_Debug_None, I2C_Device, Lepton_I2C_Command_Reboot);
+         sleep (3);
+         Lepton_App_Enable_Vsync (I2C_Device, 10, 10);
+      }
    }
 }
 
