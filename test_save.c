a6d753456542485d455e48e96c8323d5c8b3daba
KenthJohan <kenth.johan.work@gmail.com>
2018-01-22 12:52:41 +0000

    test

--- test_save.c
+++ test_save.c
@@ -24,8 +24,14 @@
 //assert
 #include <assert.h>
 
+#include <stdlib.h>
 
 
+//Creates a file.
+//The filename will be a timestamp.
+//The data being written to the file is compressed by gzip.
+//Prints the command executed by popen.
+//Returns the newly created file object.
 FILE * Opener ()
 {
    char const Format [] = "gzip - > sample_%04d_%02d_%02d_%02d_%02d_%02d.gz\n";
@@ -41,6 +47,7 @@ FILE * Opener ()
 }
 
 
+//Read an entire Pixmap from STDIN and write that data to the argument <File>.
 void Delegate (FILE * File)
 {
    struct Lepton_Pixel_Grayscale16 Pixmap [Lepton3_Width * Lepton3_Height];
@@ -57,18 +64,44 @@ void Delegate (FILE * File)
 }
 
 
+//Converts string to long.
+//Checks for error and prints them.
+//Prints successful value.
+//Returns converted value long range 1 .. LONG_MAX.
+long User_Input1 (char * Input)
+{
+   char * End;
+   long Value;
+   errno = 0;
+   Value = strtol (Input, &End, 10);
+   if (errno != 0)
+   {
+      printf ("Conversion error, non-convertable part: %s\n", End);
+   };
+   assert (errno == 0);
+   if (Value <= 0)
+   {
+      printf ("Invalid period value. Minimum 1 seconds allowed.\n");
+   }
+   assert (Value >= 1);
+   printf ("Period: %i seconds\n", (int) Value);
+   return Value;
+}
+
 
 int main (int argc, char * argv [])
 { 
+   assert (argc == 2);
+   
    int Timer;
    Timer = timerfd_create (CLOCK_MONOTONIC, TFD_NONBLOCK);
    assert (Timer > 0);
    
    struct itimerspec Spec;
-   Spec.it_interval.tv_sec = 10;
+   Spec.it_interval.tv_sec = User_Input1 (argv [1]);
    Spec.it_interval.tv_nsec = 0;
-   Spec.it_value.tv_sec = 10;
-   Spec.it_value.tv_nsec = 0;
+   Spec.it_value.tv_sec = Spec.it_interval.tv_sec;
+   Spec.it_value.tv_nsec = Spec.it_interval.tv_nsec;
    
    {
       int R = timerfd_settime (Timer, 0, &Spec, NULL);
