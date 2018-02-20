90d974a66782c101d3df2b6968a170c32f6064df
KenthJohan <kenth.johan.work@gmail.com>
2018-02-20 10:03:14 +0000

    test

---
+++ install_opencv_a.sh
@@ -0,0 +1,44 @@
+sudo apt-get remove libopencv*
+sudo apt-get autoremove
+sudo apt-get update
+sudo apt-get upgrade
+sudo rpi-update
+sudo apt-get -y install build-essential cmake cmake-curses-gui pkg-config
+sudo apt-get -y install \
+  libjpeg-dev \
+  libtiff5-dev \
+  libjasper-dev \
+  libpng12-dev \
+  libavcodec-dev \
+  libavformat-dev \
+  libswscale-dev \
+  libeigen3-dev \
+  libxvidcore-dev \
+  libx264-dev \
+  libgtk2.0-dev
+sudo apt-get -y install libv4l-dev v4l-utils
+sudo modprobe bcm2835-v4l2
+sudo apt-get -y install libatlas-base-dev gfortran
+
+cd ~
+wget https://github.com/opencv/opencv/archive/3.4.0.zip -O opencv_source.zip
+wget https://github.com/opencv/opencv_contrib/archive/3.4.0.zip -O opencv_contrib.zip
+unzip opencv_source.zip
+unzip opencv_contrib.zip
+cd opencv-3.4.0
+mkdir build
+cd build
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
+	-D CMAKE_INSTALL_PREFIX=/usr/local \
+	-D BUILD_WITH_DEBUG_INFO=OFF \
+	-D BUILD_DOCS=OFF \
+	-D BUILD_EXAMPLES=OFF \
+	-D BUILD_TESTS=OFF \
+	-D BUILD_opencv_ts=OFF \
+	-D BUILD_PERF_TESTS=OFF \
+	-D INSTALL_C_EXAMPLES=ON \
+	-D INSTALL_PYTHON_EXAMPLES=ON \
+	-D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib-3.2.0/modules \
+	-D ENABLE_NEON=ON \
+	-D WITH_LIBV4L=ON \
+        ../
