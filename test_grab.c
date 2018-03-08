e7a157cdefa8cf34d165f97e8756d23fb3b932fa
KenthJohan <kenth.johan.work@gmail.com>
2018-03-08 09:48:12 +0000

    test

--- test_grab.c
+++ test_grab.c
@@ -2,7 +2,7 @@
 
 #include "util.h"
 
-#define Lepton_Assert Assert3arg
+#define Lepton_Assert Assert_C
 
 #include "../Lepton/Lepton_SPI.h"
 #include "../Lepton/Lepton_I2C.h"
@@ -24,13 +24,13 @@
 //errrno
 #include <errno.h>
 
+//
+#include <signal.h>
+
 //INT_EDGE_RISING
 //wiringPiISR
 //wiringPiSetup
 #include <wiringPi.h>
-
-
-
 
 
 int I2C_Device;
@@ -41,7 +41,6 @@ _Atomic size_t Safe_Counter = 0;
 
 void Enable_Vsync (int Device, int Micro_Sleep, size_t Trial_Count)
 {
-   Log ("%s", "Enabling video syncronization pulse at GPIO3 pin.");
    struct Lepton_I2C_GPIO_Mode Mode;
    Mode.Value = htobe16 (Lepton_I2C_GPIO_Mode_Vsync);
    int Stage = 0;
@@ -65,14 +64,23 @@ void Enable_Vsync (int Device, int Micro_Sleep, size_t Trial_Count)
 
 void Reboot (int Device)
 {
-   int Status;
-   Status = Lepton_I2C_Status (Device);
-   Lepton_Strings_Base_printf (stderr, be16toh (Status), 10, 2, "Status: %10s\n");
-   Lepton_I2C_Write_Command (Device, Lepton_I2C_Command_Reboot);
-   sleep (3);
-   Enable_Vsync (Device, 10, 10);
-   Status = Lepton_I2C_Status (Device);
-   Lepton_Strings_Base_printf (stderr, be16toh (Status), 10, 2, "Status: %10s\n");
+	int Status;
+	Status = Lepton_I2C_Status (Device);
+	char Buffer [17] = {'\0'};
+	
+	Lepton_Strings_Base_Converter (be16toh (Status), Buffer, 16, 2);
+	Log ("Lepton device (%d): status = %16s", Device, Buffer);
+
+	Log ("Lepton device (%d): Rebooting", Device);
+	Lepton_I2C_Write_Command (Device, Lepton_I2C_Command_Reboot);
+	
+	Log ("Lepton device (%d): Enabling vsync", Device);
+	sleep (3);
+	Enable_Vsync (Device, 10, 10);
+	
+	Status = Lepton_I2C_Status (Device);
+	Lepton_Strings_Base_Converter (be16toh (Status), Buffer, 16, 2);
+	Log ("Lepton device (%d): status = %16s", Device, Buffer);
 }
 
 
@@ -80,14 +88,24 @@ void Interrupt_Handle ()
 {
    int32_t Result;
    Result = Lepton_Stream_Accept (SPI_Device, Pixmap);
+   
+   //Wait for 4th segment to arrive. 
+   //By then all 4 segment of the pixmap frame is received.
    if (Result != 4) {return;}
+   
+   //Frame received succesfully so reset Safe_Counter.
    Safe_Counter = 0;
+   
    Result = write (STDOUT_FILENO, Pixmap, sizeof (Pixmap));
+   
+   //Ignore SIGPIPE errors.
+   if (Result == -1 && errno == EPIPE) {return;}
    Assert (Result == sizeof (Pixmap), "wrote %i of %i to STDOUT_FILENO", Result, sizeof (Pixmap));
 }
 
 
-void Setup_GPIO3 ()
+//wiringPi handling the interrupt of GPIO3 vsync of FLIR Lepton.
+void Setup_wiringPi ()
 {
    int Result;
    Result = wiringPiSetup ();
@@ -99,27 +117,49 @@ void Setup_GPIO3 ()
 }
 
 
+/*
+void Signal_Handler (int Number)
+{
+	Log ("SIGPIP %d", Number);
+}
+*/
+
+
 int main (int argc, char * argv [])
 { 
-   Assert (argc == 1, "argc %i.", argc);
-   Assert (argv[0] != NULL, "argv0 %p.", argv[0]);
-   
-   I2C_Device = Lepton_I2C_Open ("/dev/i2c-1");
-   Reboot (I2C_Device);
-   SPI_Device = Lepton_SPI_Open ("/dev/spidev0.0");
-   Setup_GPIO3 ();
-   while (1)
-   {
-      sleep (1);
-      //Check if the camera goes haywire.
-      Safe_Counter ++;
-      if (Safe_Counter > 3)
-      {
-         Safe_Counter = 0;
-         Log ("%s", "Camera has gone haywire! Rebooting now to fix the problem.");
-         Reboot (I2C_Device);
-      }
-   }
+	//No argument is used currently.
+	Assert (argc == 1, "argc = %i", argc);
+	Assert (argv != NULL, "argv = %p", argv);
+
+
+	//Ignore SIGPIPE interrupt.
+	//SIGPIPE interrupt can happen when the reading end of the pipe closes.
+	signal (SIGPIPE, SIG_IGN);
+	//signal (SIGPIPE, Signal_Handler);
+
+	//I2C comm is used for setting/getting the camera registers.
+	I2C_Device = Lepton_I2C_Open ("/dev/i2c-1");
+	Reboot (I2C_Device);
+	
+	//SPI comm is only used for receiving frames.
+	SPI_Device = Lepton_SPI_Open ("/dev/spidev0.0");
+	Setup_wiringPi ();
+	
+	while (1)
+	{
+		//How frequent to check Safe_Counter.
+		sleep (1);
+		
+		//The camera is not recieving frames if the Safe_Counter starts to increase.
+		//If it goes too high then reboot the camera as it has probobly gone haywire.
+		Safe_Counter ++;
+		if (Safe_Counter > 3)
+		{
+			Safe_Counter = 0;
+			Log ("Camera has gone haywire! Rebooting now to fix the problem. I2C_Device = %d.", I2C_Device);
+			Reboot (I2C_Device);
+		}
+	}
 }
 
 
--- util.h
+++ util.h
@@ -11,31 +11,29 @@
 #define KYEL "\x1B[1;33;40m"
 #define KNRM "\x1B[0m"
 
-
-/*
-#define Log_Error(A, M, ...) fprintf (stderr, KWHT"%s:%d"KNRM": "KCYA"runtime assertion error"KNRM" '"#A"' not true!. [errno: %d, %s]. " M "\n", __FILE__, __LINE__, errno, strerror (errno), ##__VA_ARGS__);
-#define Log_Assert(A, M, ...) if (!(A)) {Log_Error (A, M, ##__VA_ARGS__); abort ();}
-#define Log(M, ...) fprintf (stderr, KWHT"%s:%d"KNRM": "KCYA"runtime log"KNRM". " M "\n", __FILE__, __LINE__, ##__VA_ARGS__);
-*/
-
-enum Util_Importance
+static inline void Util_Log
+(
+   int const Uniqid, 
+   char const * File_Name, 
+   int const Line,
+   char const * Format,
+   ...
+)
 {
-   Util_Ignore = 0,
-   Util_Info = 1,
-   Util_Abort = 2
-};
+	va_list Ap;
+	va_start (Ap, Format);
+	fprintf (stderr, "" KYEL "%04d. " KNRM "" KWHT "%s:%d" KNRM ": " KMAG "runtime log" KNRM ". ", Uniqid, File_Name, Line);
+	vfprintf (stderr, Format, Ap);
+	fprintf (stderr, "\n");
+	va_end (Ap);
+}
 
-enum Util_Code
-{
-   Util_Normal = __COUNTER__
-};
 
-static void Util_Logger 
+static inline void Util_Assert 
 (
    int const Uniqid, 
    int const Code,
    char const * Code_String, 
-   int const Importance,
    char const * Assertion,
    char const * Function_Name, 
    char const * File_Name, 
@@ -44,48 +42,30 @@ static void Util_Logger
    ...
 )
 {
-   va_list Ap;
-   va_start (Ap, Format);
-   switch (Importance)
-   {
-      case Util_Ignore:
-         break;
-      
-      case Util_Info:
-         fprintf (stderr, "" KYEL "%04d. " KNRM "" KWHT "%s:%d" KNRM ": " KMAG "runtime log" KNRM ". ", Uniqid, File_Name, Line);
-         vfprintf (stderr, Format, Ap);
-         fprintf (stderr, "\n");
-         break;
-      
-      case Util_Abort:
-         fprintf (stderr, "Uniq Id           : %i\n", Uniqid);
-         fprintf (stderr, "Code              : %i = %s\n", Code, Code_String);
-         fprintf (stderr, "Importance        : %i\n", Importance);
-         fprintf (stderr, "Assertion         : %s\n", Assertion);
-         fprintf (stderr, "Function_Name     : %s\n", Function_Name);
-         fprintf (stderr, "File_Name         : %s\n", File_Name);
-         fprintf (stderr, "Line              : %i\n", Line);
-         fprintf (stderr, "errno             : %i\n", errno);
-         fprintf (stderr, "strerror          : %s\n", strerror (errno));
-         vfprintf (stderr, Format, Ap);
-         fprintf (stderr, "\n\n");
-         abort ();
-         break;
-      
-      default:
-         break;
-   };
-   va_end (Ap);
+	va_list Ap;
+	va_start (Ap, Format);
+	fprintf (stderr, "Uniq Id           : %i\n", Uniqid);
+	fprintf (stderr, "Code              : %i = %s\n", Code, Code_String);
+	fprintf (stderr, "Assertion         : %s\n", Assertion);
+	fprintf (stderr, "Function_Name     : %s\n", Function_Name);
+	fprintf (stderr, "File_Name         : %s\n", File_Name);
+	fprintf (stderr, "Line              : %i\n", Line);
+	fprintf (stderr, "errno             : %i\n", errno);
+	fprintf (stderr, "strerror          : %s\n", strerror (errno));
+	vfprintf (stderr, Format, Ap);
+	fprintf (stderr, "\n\n");
+	abort ();
+	va_end (Ap);
 }
 
-#define Assert3arg(A, Code, Message, ...) \
-if (!(A)) {Util_Logger(__COUNTER__, Code, #Code, Util_Abort, #A, __func__, __FILE__, __LINE__, Message, __VA_ARGS__); }
+
+#define Util_Assertion 0
+
+#define Assert_C(A, Code, Message, ...) \
+if (!(A)) {Util_Assert(__COUNTER__, Code, #Code, #A, __func__, __FILE__, __LINE__, Message, __VA_ARGS__); }
 
 #define Assert(A, Message, ...) \
-if (!(A)) {Util_Logger(__COUNTER__, Util_Normal, "Util_Normal", Util_Abort, #A, __func__, __FILE__, __LINE__, Message, __VA_ARGS__); }
+if (!(A)) {Assert_C (A, Util_Assertion, Message, __VA_ARGS__)}
 
 #define Log(Message, ...) \
-Util_Logger(__COUNTER__, Util_Normal, "Util_Normal", Util_Info, "", __func__, __FILE__, __LINE__, Message, __VA_ARGS__)
-
-
-
+Util_Log(__COUNTER__, __FILE__, __LINE__, Message, __VA_ARGS__)
